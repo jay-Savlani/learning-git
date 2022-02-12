32fbce623bf56491b40eeeb294b09f6fb79f76a2
Jay Savlani <jaysavlani2608@gmail.com>
2022-02-12 21:31:35 +0530

    Changes meant for featureA branch

--- example2.js
+++ example2.js
@@ -1,3 +1,4 @@
 const example2 = () => {
     console.log("example 2 js file created");
+    console.log("This changes were meant to be for featureA branch but accidently got added in master branch")
 }
